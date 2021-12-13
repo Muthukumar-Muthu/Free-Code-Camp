83166e210d3a851d6c1adc41c49da0586deeebaa
Muthukumar <m.m.muthu1388@gmail.com>
2021-12-13 12:01:50 +0530

    updated script with full functionality

--- FAQ/script.js
+++ FAQ/script.js
@@ -1,39 +1,39 @@
+/*
 const question = document.querySelector(".content-container");
 
 question.addEventListener("click", function(e) {
-    console.log(e.type);
+    // console.log(e.type);
     const element = e.target;
     const child = e.target.parentElement.parentElement;
     const ans = child.children[1];
     if (e.target.classList.contains("fa-expand-arrows-alt")) {
         ans.classList.toggle("hide");
-        console.log(ans.classList);
+        // console.log(ans.classList);
         if (ans.classList.contains("hide")) {
-            console.log("hiding");
+            // console.log("hiding");
             ans.style.maxHeight = "0";
         } else ans.style.maxHeight = "200px";
     }
 });
+ */
 
-/*
-const question = document.querySelector(".content-container");
+const singleQuestion = document.querySelectorAll(".single-question");
+singleQuestion.forEach(function(value, index) {
+    const expand = value.firstElementChild.lastElementChild;
 
-question.addEventListener("click", function(e) {
-    console.log(e.type);
-    const element = e.target;
-    const child = e.target.parentElement.parentElement;
-    const ans = child.children[1];
-    if (e.target.classList.contains("fa-expand-arrows-alt")) {
-        ans.style.maxHeight = "200px";
-    }
+    expand.addEventListener("click", function(e) {
+        check();
+        const ans = e.target.parentElement.parentElement.lastElementChild;
+
+        ans.classList.toggle("toggle");
+    });
 });
-question.addEventListener("dblclick", function(e) {
-    console.log(e.type);
-    const element = e.target;
-    const child = e.target.parentElement.parentElement;
-    const ans = child.children[1];
-    if (e.target.classList.contains("fa-expand-arrows-alt")) {
-        ans.style.maxHeight = "0";
-    }
-});
-*/
+
+function check() {
+    const ans = document.querySelectorAll(".answer-container");
+    ans.forEach(function(value, index) {
+        if (value.classList.contains("toggle")) {
+            value.classList.remove("toggle");
+        }
+    });
+}
