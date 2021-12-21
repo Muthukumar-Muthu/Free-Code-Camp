6689b47312f222fff5b83ef34a429c3c4766c264
Muthukumar <m.m.muthu1388@gmail.com>
2021-12-21 10:41:55 +0530

    added up-arrow button and fixed position

--- Scroll/script.js
+++ Scroll/script.js
@@ -5,6 +5,7 @@ const header = document.querySelector("header");
 const nav = document.querySelector("nav");
 const a = document.querySelectorAll("a");
 const id = document.querySelectorAll(".scroll-links");
+const upArrow = document.querySelector("#up");
 
 window.addEventListener("load", () => {
     linksContainer.style.height = 0;
@@ -22,21 +23,31 @@ window.addEventListener("scroll", () => {
     if (window.pageYOffset > navHeight) {
         nav.classList.add("fixed");
     } else nav.classList.remove("fixed");
+    if (window.pageYOffset > 100) {
+        upArrow.style.visibility = "visible";
+    } else upArrow.style.visibility = "hidden";
 });
+
+upArrow.addEventListener("click", () => {
+    const title1 = document.querySelector("#title1");
+    let position = title1.offsetTop;
+    position = position - header.getBoundingClientRect().height;
+    window.scrollTo({
+        left: 0,
+        top: position,
+    });
+});
+
 a.forEach((value) => {
     value.addEventListener("click", (e) => {
         e.preventDefault();
         const id = e.target.getAttribute("href");
         const elementId = document.querySelector(id);
-        let height = elementId.offsetTop;
-        const navHeight = nav.getBoundingClientRect().height;
-        height = height - navHeight;
-        if (window.screen.width < 700) {
-            height = height + linksContainer.getBoundingClientRect().height;
-        }
+        let position = elementId.offsetTop;
+        position = position - header.getBoundingClientRect().height;
         window.scrollTo({
             left: 0,
-            top: height,
+            top: position,
         });
         linksContainer.style.height = 0;
         // close
